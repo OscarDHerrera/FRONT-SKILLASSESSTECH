2ab814ee8b36085af4f9add9ba6f1a80fcf410af
oscar.herrerag <osda26@gmail.com>
2022-09-22 16:08:35 -0500

    Ajuste al responsive de la vista Questions
    Se hacen los cambios para mejorar el responsive en dispositivos moviles de la vista Questions

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -5,7 +5,7 @@ import {
   Grid,
   Typography,
   Paper,
-  Switch 
+  Switch
 } from '@mui/material'
 
 const GreenSwitch = styled(Switch)(({ theme }) => ({
@@ -44,26 +44,24 @@ export default function Questions() {
 
   return (
     <Container fixed>
-      {
-        myQuestions.map((item, index) => (
-          <Paper
-            key={index}
-            sx={{
-              my: 1,
-              mx: 'auto',
-              p: 2,
-            }}
-          >
-            <Grid container wrap="nowrap" spacing={2}>
-              <Grid item>
-              <GreenSwitch {...label} defaultChecked />
+      <Grid spacing={{ xs: 2, md: 2 }} columns={{ xs: 2, sm: 2, md: 12 }}>
+        {
+          myQuestions.map((item, index) => (
+            <Paper
+              key={index}
+              elevation={3}
+              sx={{
+                my: 1,
+                mx: 'auto',
+                p: 2,
+              }}
+            >
+              <Grid item key={index} xs={3} sm={4} md={4}>
+                <Typography><GreenSwitch {...label} /> {item.question}</Typography>
               </Grid>
-              <Grid item xs>
-                <Typography noWrap>{item.question}</Typography>
-              </Grid>
-            </Grid>
-          </Paper>
-        ))}
+            </Paper>
+          ))}
+      </Grid>
     </Container>
   )
 }
