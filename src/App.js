4d5ac61df192032222f24f5a86177697913bb2b5
oscar.herrerag <osda26@gmail.com>
2022-10-21 15:35:42 -0500

    Mejoras de identación, se agregó Footer en App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import BarraNav from './layouts/BarraNav';
 import Home from './components/home/Home';
 import FormPerson from './components/users/FormPerson';
 import UserTable from './components/users/UserTable';
 import Questions from './components/home/Questions';
-
+import SubCategories from './components/home/SubCategories';
+import Footer from './layouts/Footer'
+import BarraNav from './layouts/BarraNav';
 
 export default function App() {
+
   return (
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route exact path='/' element={<BarraNav/>}>
-              <Route exact path='/' element = {<Home/>}/>
-              <Route exact path='/select-items' element = {<Questions/>}/>
-              <Route exact path='/create-user' element={<FormPerson/>} />
-              <Route exact path='/users-table' element={<UserTable/>} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
+    <div className="App">
+      <BarraNav />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/subcategories/:sub_name' element={<SubCategories />} />
+          <Route exact path='/subcategories/questions/:sub_name' element={<Questions />} />
+          <Route exact path='/create-user' element={<FormPerson />} />
+          <Route exact path='/users-table' element={<UserTable />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
--- src/commons/AppAlert.jsx
+++ src/commons/AppAlert.jsx
@@ -1,28 +1,33 @@
 import * as React from 'react';
 import {
-    Snackbar,
+  Snackbar,
 } from '@mui/material';
 import MuiAlert from '@mui/material/Alert';
 
 const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export function AppAlert({ showAlert, handleCloseAlert, severityResponse, messageResponse }) {
-    return (
-        <Snackbar open={showAlert} autoHideDuration={6000} onClose={handleCloseAlert}>
-            {
-                severityResponse === "error" ? (
-                    <Alert onClose={handleCloseAlert} severity={`${severityResponse}`} sx={{ width: '100%', bgcolor: '#ff1837' }}>
-                        {messageResponse}
-                    </Alert>
-                ) :
-                    (
-                        <Alert onClose={handleCloseAlert} severity={`${severityResponse}`} sx={{ width: '100%' }}>
-                            {messageResponse}
-                        </Alert>
-                    )
-            }
-        </Snackbar>
-    )
+export function AppAlert({ showAlert, handleCloseAlert, severityResponse, messageResponse, colorMessage }) {
+  return (
+    <Snackbar open={showAlert} autoHideDuration={6000} onClose={handleCloseAlert}>
+      {
+        severityResponse === "error" ? (
+          <Alert onClose={handleCloseAlert} severity={`${severityResponse}`} sx={{ width: '100%', bgcolor: '#ff1837' }}>
+            {messageResponse}
+          </Alert>
+        ) : severityResponse === "success" ?
+          (
+            <Alert onClose={handleCloseAlert} severity={`${severityResponse}`} sx={{ width: '100%' }}>
+              {messageResponse}
+            </Alert>
+          ) :
+          (
+            <Alert onClose={handleCloseAlert} severity={`${severityResponse}`} sx={{ width: '100%' }}>
+              {messageResponse}
+            </Alert>
+          )
+      }
+    </Snackbar>
+  )
 }
